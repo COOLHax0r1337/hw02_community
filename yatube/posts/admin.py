4b44a49a92c838a2d5a354de4ef2187705d30154
Philip Kurtakov <zj6lyp03r9oc@mail.ru>
2022-11-16 13:22:25 +0300

    pepflake again

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import Post, Group
 # Register your models here.
 
-#admin.site.register(Post)
+# admin.site.register(Post)
+
 
 class PostAdmin(admin.ModelAdmin):
     list_display = (
